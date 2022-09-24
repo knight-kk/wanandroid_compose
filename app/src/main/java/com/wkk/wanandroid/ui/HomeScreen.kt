8d49669e4cc3d6868aee956eac72e6e6d067b34a
knight-kk <2803100746@qq.com>
2022-09-24 09:46:14 +0800

    首页添加列表收藏按钮

--- app/src/main/java/com/wkk/wanandroid/ui/HomeScreen.kt
+++ app/src/main/java/com/wkk/wanandroid/ui/HomeScreen.kt
@@ -18,14 +18,25 @@ package com.wkk.wanandroid.ui
 import android.text.format.DateFormat
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Star
 import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SmallTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
 import com.wkk.wanandroid.model.Article
@@ -36,7 +47,11 @@ import com.wkk.wanandroid.vm.HomeViewModel
  */
 @Composable
 fun HomeScreen(viewModel: HomeViewModel, onItemClick: (Article) -> Unit) {
-    ArticleList(viewModel, onItemClick)
+    Scaffold(topBar = { SmallTopAppBar(title = { Text(text = "首页") }) }) { paddingValues ->
+        Column(Modifier.padding(paddingValues)) {
+            ArticleList(viewModel, onItemClick)
+        }
+    }
 }
 
 @Composable
@@ -54,19 +69,36 @@ fun ArticleList(viewModel: HomeViewModel, onItemClick: (Article) -> Unit) {
 
 @Composable
 fun ArticleItem(article: Article, onItemClick: (Article) -> Unit) {
-    Column(
-        Modifier
-            .clickable(onClick = { onItemClick(article) })
-            .padding(10.dp)
-    ) {
-        Text(text = article.title)
-        Text(text = article.author)
-        Text(text = DateFormat.format("yyyy-MM-dd HH:mm:ss", article.publishTime).toString())
-    }
+    val (isCollection, setCollection) = remember { mutableStateOf(article.collect) }
+    ListItem(
+        modifier = Modifier.fillMaxWidth().clickable(onClick = { onItemClick(article) }),
+        overlineText = {
+            Text(text = article.author.takeIf { it.isNotEmpty() } ?: article.shareUser)
+        },
+        headlineText = { Text(text = article.title) },
+        supportingText = {
+            Text(text = DateFormat.format("yyyy-MM-dd HH:mm:ss", article.publishTime).toString())
+        },
+        leadingContent = {
+            IconButton(onClick = { setCollection(isCollection.not()) }) {
+                Icon(
+                    imageVector = Icons.Rounded.Star,
+                    tint = if (isCollection) MaterialTheme.colorScheme.primary else Color.Black,
+                    contentDescription = "collection"
+                )
+            }
+        }
+    )
 }
 
 @Preview(showBackground = false)
 @Composable
-fun HomePreView() {
-    ArticleItem(Article()) {}
+fun ArticleItemPreview() {
+    ArticleItem(
+        Article(
+            title = "文章标题",
+            author = "knight-kk",
+            publishTime = System.currentTimeMillis()
+        )
+    ) {}
 }
--- app/src/main/java/com/wkk/wanandroid/ui/theme/Theme.kt
+++ app/src/main/java/com/wkk/wanandroid/ui/theme/Theme.kt
@@ -28,7 +28,7 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 
 /**
  * 由 [Material Theme Builder](https://m3.material.io/theme-builder#/custom) 生成
@@ -61,7 +61,7 @@ private val LightColorScheme = lightColorScheme(
     inverseOnSurface = md_theme_light_inverseOnSurface,
     inverseSurface = md_theme_light_inverseSurface,
     inversePrimary = md_theme_light_inversePrimary,
-    surfaceTint = md_theme_light_surfaceTint,
+    surfaceTint = md_theme_light_surfaceTint
 )
 
 private val DarkColorScheme = darkColorScheme(
@@ -91,14 +91,14 @@ private val DarkColorScheme = darkColorScheme(
     inverseOnSurface = md_theme_dark_inverseOnSurface,
     inverseSurface = md_theme_dark_inverseSurface,
     inversePrimary = md_theme_dark_inversePrimary,
-    surfaceTint = md_theme_dark_surfaceTint,
+    surfaceTint = md_theme_dark_surfaceTint
 )
 
 @Composable
 fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -106,14 +106,18 @@ fun AppTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
+
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            (view.context as Activity).window?.apply {
+                statusBarColor = colorScheme.surface.toArgb()
+                WindowCompat.getInsetsController(this, view).isAppearanceLightStatusBars =
+                    !darkTheme
+            }
         }
     }
 
