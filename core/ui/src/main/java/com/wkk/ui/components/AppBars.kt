379e06d2377c977ef90125bf05aa42553f0b82fc
knight-kk <2803100746@qq.com>
2023-08-05 23:36:21 +0800

    添加 公共 components

---
+++ core/ui/src/main/java/com/wkk/ui/components/AppBars.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2023 knight-kk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.wkk.ui.components
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+
+fun appTopBar(title: String, onNavigation: () -> Unit): @Composable () -> Unit = {
+    AppTopBar(title, onNavigation)
+}
+
+@Composable
+fun AppTopBar(title: String, onNavigation: () -> Unit) {
+    TopAppBar(
+        title = { Text(text = title) },
+        navigationIcon = {
+            IconButton(onClick = onNavigation) {
+                Icon(imageVector = Icons.Default.ArrowBack, "back")
+            }
+        },
+    )
+}
--- core/ui/src/main/java/com/wkk/ui/components/WebView.kt
+++ core/ui/src/main/java/com/wkk/ui/components/WebView.kt
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.wkk.ui.theme.components
+package com.wkk.ui.components
 
 import android.annotation.SuppressLint
 import android.webkit.WebView
